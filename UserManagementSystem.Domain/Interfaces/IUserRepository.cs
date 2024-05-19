25c5fa0e412ae66d05e0b87bc3008598c936f2b3
ahmad364 <ahmadfiverr03034@gmail.com>
2024-05-19 17:37:00 +0500

    Completed domain layer

---
+++ UserManagementSystem.Domain/Interfaces/IUserRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UserManagementSystem.Domain.Entities;
+
+namespace UserManagementSystem.Domain.Interfaces
+{
+    public interface IUserRepository
+    {
+        Task<User> GetUserByIdAsync(int id);
+        Task<IEnumerable<User>> GetAllUsersAsync();
+        Task AddUserAsync(User user);
+        Task UpdateUserAsync(User user);
+        Task DeleteUserAsync(int id);
+    }
+}
